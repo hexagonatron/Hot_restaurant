cbdb25fcfdb4390cb1ce6ff82179197571fbc3d6
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-05-04 21:05:30 +0930

    Finished

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const tableArr = [];
 const waitListArr = [];
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.listen(PORT, function() {
@@ -19,19 +19,37 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-
-
-
 app.get("/reserve", (req, res) => {
     res.sendFile(path.join(__dirname, "reserve.html"));
 })
 
-app.get("/tables", (req, res) => {
-    res.sendFile(path.join(__dirname, "tables.html"));
+app.get("/api/tables", (req, res) => {
+    res.json(tableArr);
 })
 
-app.post("/reserve", (req, res) => {
-    const body = req.body;
-    res.sendFile(path.join(__dirname, "reserve.html"));
-    console.log(body);
+app.get("/api/waitlist", (req, res) => {
+    res.json(waitListArr);
 })
+
+app.delete("/api/clear", (req, res) => {
+    res.send();
+    tableArr.length = 0;
+    waitListArr.length = 0;
+})
+
+app.post("/api/reservation", (req, res) => {
+    const reservation = req.body;
+    if(tableArr.length < 5){
+        tableArr.push(reservation);
+    } else {
+        waitListArr.push(reservation);
+    }
+    res.send(JSON.stringify(tableArr));
+});
+
+app.get("/api/reservation", (req, res) => {
+    res.json({
+        tables: tableArr,
+        waitlist: waitListArr
+    })
+});
